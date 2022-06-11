25338eed155a534d5306baf5405a799b4cae594a
Bhuvanesh <bhuvanesh.c@synamen.com>
2022-06-11 17:15:04 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+## Multiple Image Upload in Ionic 6
+
+Hi Guys, In this example i will show you how to uploabhuvaneshcj/Qrcode-Scanner-Using-Instascan-Plugind multiple image in Ionic 6 with additonal options Reorder and Delete ( specific image ).
+
+## Screenshots
+
+![App Screenshot](https://github.com/bhuvaneshcj/Multiple-Image-Upload-in-Ionic-6/images/screenshot-I.png)
+
+## Getting Started
+
+#### 1. Clone the repository
+
+```
+git clone https://github.com/bhuvaneshcj/Multiple-Image-Upload-in-Ionic-6.git --branch=master
+```
+
+### 2. Install the node packages by using the below command inside the project folder
+
+> check your node version should be 16.13.1 before installing node package
+
+```
+npm install
+```
+
+### 3. After the node package installation run the project
+
+```
+ionic serve
+```
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { IonReorderGroup } from '@ionic/angular';
+import { ItemReorderEventDetail } from '@ionic/core';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,49 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  selectedFiles?: any = [];
+  progressInfos: any[] = [];
+  message: string[] = [];
+  previews: string[] = [];
+  product_id: number;
+
+  @ViewChild(IonReorderGroup) reorderGroup: IonReorderGroup;
+  @ViewChild('image') input: ElementRef;
 
   constructor() {}
 
+  selectFiles(event: any): void {
+    this.message = [];
+    this.progressInfos = [];
+    this.selectedFiles.push(...event.target.files);
+    this.previews = [];
+    const numberOfFiles = this.selectedFiles.length;
+
+    for (let i = 0; i < numberOfFiles; i++) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.previews.push(e.target.result);
+      };
+      console.log(this.selectedFiles[i]);
+      reader.readAsDataURL(this.selectedFiles[i]);
+    }
+  }
+
+  doReorder(ev: CustomEvent<ItemReorderEventDetail>) {
+    this.previews = ev.detail.complete(this.previews);
+    var newArr = this.selectedFiles;
+    [newArr[ev.detail.from], newArr[ev.detail.to]] = [
+      newArr[ev.detail.to],
+      newArr[ev.detail.from],
+    ];
+  }
+
+  toggleReorderGroup() {
+    this.reorderGroup.disabled = !this.reorderGroup.disabled;
+  }
+
+  removeImage(a) {
+    this.previews.splice(a, 1);
+    this.selectedFiles.splice(a, 1);
+  }
 }
